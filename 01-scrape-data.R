9a0489b368cb7c4ca244fe360be30d6905c0063a
faisal-samin <fasamin@gmail.com>
2018-10-16 19:40:12 +0100

    complete scraping and transformation

--- 01-scrape-data.R
+++ 01-scrape-data.R
@@ -1,5 +1,4 @@
-# We first need to extract the required data from data dumps hosted by the FPL site.
-# There are two APIs that are used to extract fantasy football data:
+# We first need to extract the required data from data dumps hosted by the FPL site:
 
 # https://fantasy.premierleague.com/drf/elements/: Contains player IDs, names and other general details
 # https://fantasy.premierleague.com/drf/element-summary/{id}: contains detailed stats on footballers
@@ -8,18 +7,16 @@
 # Modules and import data -------------------------------------------------
 
 library(tidyverse)
-library(jsonlite)
+library(jsonlite) # flattening jsons
+library(tictoc) # timing code
 
 # fromJSON flattens the json file
 elements <- fromJSON("https://fantasy.premierleague.com/drf/elements/")
-View(elements)
 
 # note that 'now_cost' and 'selected_by_percent' will change frequently
-colnames(elements)
 
 # while there are some general stats here (like goal_scored), these aren't split by gameweek
 # we only want general details from this json
-
 
 # Data transformation -----------------------------------------------------
 
@@ -28,7 +25,7 @@ colnames(elements)
 # some players have more than one first name, we want to only select the first one
 # this only affects surnames but it's more difficult to arbitrarily select the correct name
 
-player_summary = elements %>%
+players = elements %>%
   mutate(name = 
            paste(word(first_name, 1), 
                  second_name, 
@@ -64,10 +61,83 @@ player_summary = elements %>%
   arrange(id, team, element_type) %>%
   select(id, name, now_cost, selected_by_percent, team, position, photo)
 
-# We now join onto gameweek level from the second API
+# We now join onto gameweek level from the detailed URL dumps
 
-# Using hazard as an example
-hazard = fromJSON("https://fantasy.premierleague.com/drf/element-summary/122")
+# Initialise an empty data frame
+gameweek_details = data.frame()
 
-select(hazard$history, ''
+tic("Scraping: ") # Takes approximately 86 seconds
+for (i in players$id) {
+  url = fromJSON(
+    paste("https://fantasy.premierleague.com/drf/element-summary/",
+              toString(i), sep = ""))
+  history = url$history # gameweek history
+  gameweek_details = rbind(gameweek_details, history) 
+  print(i)
+  # Sys.sleep(1)
+}
+gameweek_details = subset(gameweek_details, select = -id) # drop id column
+toc()
+
+# Finally, we join the two dataframes
+fpl <- merge(players, gameweek_details, by.x = "id", by.y = "element")
+
+filter(fpl, name == "Eden Hazard") %>%
+  select(photo)
+
+# Further tranformations --------------------------------------------------
+# We add a few final touches, and refine the columns for our final dataset
+
+# Fixing image URLs
+fpl$image_url = paste("https://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/p",
+                      strsplit(fpl$photo, split = ".", fixed = TRUE)[[1]][1],
+                      ".png",
+                      sep = "")
+
+write_csv(fpl, "fpl.csv")
+
+# Reordering columns and dropping those not of interest
+fpl <- select(fpl,
+              id, name, team, position,
+              selected, now_cost, value,
+              round, 
+              team_h_score, team_a_score, was_home,                       
+              total_points,
+              selected, transfers_in, transfers_out,          
+              minutes,                      
+              goals_scored,                  
+              assists,
+              clean_sheets,
+              goals_conceded,                 
+              own_goals,                     
+              penalties_saved,                
+              penalties_missed,               
+              yellow_cards,                   
+              red_cards,                      
+              saves,                         
+              bonus,                         
+              bps,                            
+              open_play_crosses,             
+              big_chances_created,           
+              clearances_blocks_interceptions,
+              recoveries,                    
+              key_passes,                   
+              tackles,                     
+              winning_goals,                  
+              attempted_passes,               
+              completed_passes,             
+              penalties_conceded,           
+              big_chances_missed,            
+              errors_leading_to_goal,         
+              errors_leading_to_goal_attempt,
+              tackled,                        
+              offside,                       
+              target_missed,                  
+              fouls,                        
+              dribbles,                     
+              fixture,                       
+              opponent_team,                  
+              image_url)
+
+
 
